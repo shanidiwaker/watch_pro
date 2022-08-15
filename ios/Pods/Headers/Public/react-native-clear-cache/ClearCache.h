46929814700a2b87e02ff19657b5a1f6b09ea14b
Nikhil Jain <njain923@gmail.com>
2022-08-15 14:24:00 +0530

    test

--- ios/Pods/Headers/Public/react-native-clear-cache/ClearCache.h
+++
@@ -1,1 +0,0 @@
-../../../../../node_modules/react-native-clear-cache/ios/ClearCache.h
--- ios/Pods/Target Support Files/Pods-watchpro-watchproTests/Pods-watchpro-watchproTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_watchpro_watchproTests : NSObject
-@end
-@implementation PodsDummy_Pods_watchpro_watchproTests
-@end
